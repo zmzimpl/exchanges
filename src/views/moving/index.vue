6c389fbd4a3eb6f26c6b09287c8389a152c33b07
zhangmingzeng <461045745@qq.com>
2019-07-24 15:36:32 +0800

    完善搬砖界面功能

--- src/views/moving/index.vue
+++ src/views/moving/index.vue
@@ -2,14 +2,14 @@
   <div class="moving-container">
     <el-row :gutter="24">
       <el-col :xs="24" :xl="16" :span="16">
-        <el-card>
+        <el-card v-for="(moveForm, moveIndex) in moves" :key="moveIndex" :class="{ 'moving-card' : moveIndex !== 0 }">
           <div slot="header" class="clearfix">
             <div class="moving-center">
               <span>当前币种：</span>
               <span>收益：</span>
             </div>
           </div>
-          <el-form ref="dynamicValidateForm" :model="dynamicValidateForm" label-width="100px" class="moving-form">
+          <el-form :ref="moveIndex" :model="moveForm" label-width="100px" class="moving-form">
             <el-form-item
               prop="currency"
               label="币种"
@@ -17,7 +17,7 @@
                 { required: true, message: '请选择币种', trigger: 'change' }
               ]"
             >
-              <el-select v-model="dynamicValidateForm.currency" placeholder="请选择交易币种" class="moving-select">
+              <el-select v-model="moveForm.currency" placeholder="请选择交易币种" class="moving-select">
                 <el-option
                   v-for="item in options"
                   :key="item.value"
@@ -27,7 +27,7 @@
               </el-select>
             </el-form-item>
             <el-form-item
-              v-for="(account, index) in dynamicValidateForm.accounts"
+              v-for="(account, index) in moveForm.accounts"
               :key="account.key"
               :label="'API账号' + index"
               :prop="'accounts.' + index + '.value'"
@@ -36,23 +36,42 @@
               }"
             >
               <el-cascader v-model="account.value" :options="apiAccounts" class="moving-select" />
-              <el-button @click.prevent="removeAccount(account)">移除</el-button>
+              <el-button @click.prevent="removeAccount(moveForm, account)">移除</el-button>
             </el-form-item>
             <el-form-item class="moving-center">
-              <el-button type="primary" @click="submitForm('dynamicValidateForm')">开始</el-button>
-              <el-button @click="addAccount">新增API账号</el-button>
-              <el-button type="danger" @click="resetForm('dynamicValidateForm')">删除</el-button>
+              <el-button type="primary" @click="submitForm('moveForm')">开始</el-button>
+              <el-button @click="addAccount(moveForm)">新增API账号</el-button>
+              <el-button type="danger" v-if="moves.length > 1" @click="removeForm(moveIndex)">删除</el-button>
             </el-form-item>
           </el-form>
         </el-card>
       </el-col>
       <el-col :xs="0" :xl="8" :span="8">
-        <div class="grid-content bg-purple-light" />
+        <el-table
+          :data="tableData"
+          stripe
+          border
+          style="width: 100%">
+          <el-table-column
+            prop="date"
+            label="时间"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="exchange"
+            label="交易所"
+            width="180">
+          </el-table-column>
+          <el-table-column
+            prop="profit"
+            label="盈利">
+          </el-table-column>
+        </el-table>
       </el-col>
     </el-row>
     <el-row type="flex" justify="center">
       <el-col :span="16" class="moving-center" style="margin: 24px 0">
-        <el-button round style="width: 40%">
+        <el-button round style="width: 40%" @click="addMove()">
           <i class="el-icon-plus" /> 新建
         </el-button>
       </el-col>
@@ -284,6 +303,22 @@ export default {
           value: 'jiaohu',
           label: '组件交互文档'
         }]
+      }],
+      moves: [{
+        accounts: [{
+          key: Date.now(),
+          value: []
+        }],
+        currency: ''
+      }],
+      tableData: [{
+        date: '2019-05-02',
+        exchange: '币安',
+        profit: '0.5%'
+      }, {
+        date: '2019-05-04',
+        exchange: '火币Pro',
+        profit: '0.3%'
       }]
     }
   },
@@ -298,19 +333,28 @@ export default {
         }
       })
     },
-    resetForm(formName) {
-      this.$refs[formName].resetFields()
+    removeForm(moveIndex) {
+      this.moves.splice(moveIndex, 1);
     },
-    removeAccount(item) {
-      var index = this.dynamicValidateForm.accounts.indexOf(item)
+    removeAccount(moveForm, item) {
+      var index = moveForm.accounts.indexOf(item)
       if (index !== -1) {
-        this.dynamicValidateForm.accounts.splice(index, 1)
+        moveForm.accounts.splice(index, 1)
       }
     },
-    addAccount() {
-      this.dynamicValidateForm.accounts.push({
+    addAccount(form) {
+      form.accounts.push({
         value: [],
         key: Date.now()
+      })
+    },
+    addMove() {
+      this.moves.push({
+        accounts: [{
+          key: Date.now(),
+          value: []
+        }],
+        currency: ''
       })
     }
   }
@@ -338,6 +382,9 @@ export default {
   }
   &-center {
     text-align: center
+  }
+  &-card {
+    margin-top: 16px;
   }
 }
 </style>
